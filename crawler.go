855922ca932a93b166611ba95469ce4da39c175d
Junior M <juniorfvox@gmail.com>
2022-03-09 01:12:16 -0300

    feat: add func to spawn workers.

--- crawler.go
+++ crawler.go
@@ -1,20 +1,42 @@
 package main
 
+import (
+	"log"
+	"sync"
+)
+
 type Crawler struct {
 	MaxWorkers int
 	jobChan    chan Job
+	waitGroup  *sync.WaitGroup
 }
 
 func NewCrawler(maxWorkers int) *Crawler {
 	jobChan := make(chan Job)
+	wg := &sync.WaitGroup{}
 
 	return &Crawler{
 		MaxWorkers: maxWorkers,
 		jobChan:    jobChan,
+		waitGroup:  wg,
+	}
+}
+
+func (c *Crawler) SpawnWorkers() {
+	for i := 1; i <= c.MaxWorkers; i++ {
+		go func(workerId int) {
+			for job := range c.jobChan {
+				log.Printf("Job: %+v\n", job)
+
+				c.waitGroup.Done()
+			}
+		}(i)
 	}
 }
 
 func (c *Crawler) Enqueue(url string) {
+	c.waitGroup.Add(1)
+
 	go func() {
 		job := Job{Url: url}
 		c.jobChan <- job
--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 func main() {
 	// Initialize crawler
 	crawler := NewCrawler(4)
+	crawler.SpawnWorkers()
 
 	crawler.Enqueue("http://github.com")
 
 	log.Printf("Crawler: %+v\n", crawler)
+
+	crawler.Wait()
+
+	log.Println("Finished")
 }
